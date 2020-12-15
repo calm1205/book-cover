c70146e9475cffae55689135ab043494ad442ede
calm1205 <naginani072@gmail.com>
2020-12-15 16:38:21 +0900

    make cover controller

--- app/controllers/covers_controller.rb
+++ app/controllers/covers_controller.rb
@@ -1,4 +1,20 @@
 class CoversController < ApplicationController
   def index
   end
+
+  def pdf
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'test_format',
+          layout: 'pdf.html.erb',
+          template: 'covers/sample.html.erb',
+          page_size: 'A3',
+          orientation: 'Landscape',
+          encoding: 'UTF-8',
+          show_as_html: params[:debug]
+      end
+    end
+  end
+
 end
